df0eac85b93981aeadee16aba9921e8c6153ebff
mhmmdd <mhmmd.dd@gmail.com>
2021-11-17 17:52:29 +0300

    Spanish language property

--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -3,9 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// By doing this when it fails the line number reported will be in our function call
+		// rather than inside our test helper.
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
