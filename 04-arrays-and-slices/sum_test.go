ba403030164571414dd338faffa5da5627adc7e6
mhmmdd <mhmmd.dd@gmail.com>
2021-11-19 11:25:10 +0300

    add 04-arrays-and-slices

---
+++ 04-arrays-and-slices/sum_test.go
@@ -0,0 +1,27 @@
+package array_and_slice
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	//if got != want {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
