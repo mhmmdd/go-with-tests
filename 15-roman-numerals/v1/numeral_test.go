fc8f0cedf5efc166882a68ac7f6077d58c648175
mhmmdd <mhmmd.dd@gmail.com>
2021-11-27 17:33:32 +0300

    add 15-roman-numerals

---
+++ 15-roman-numerals/v1/numeral_test.go
@@ -0,0 +1,27 @@
+package roman_numerals
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	t.Run("1 gets converted to I", func(t *testing.T) {
+		got := ConvertToRoman(1)
+		want := "I"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("2 gets converted to II", func(t *testing.T) {
+		got := ConvertToRoman(2)
+		want := "II"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func ConvertToRoman(arabic int) string {
+	if arabic == 2 {
+		return "II"
+	}
+	return "I"
+}
---
+++ 15-roman-numerals/v5/numeral_test.go
@@ -0,0 +1,46 @@
+package roman_numerals
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+	}
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
+}
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
+	}
+	return result.String()
+}
